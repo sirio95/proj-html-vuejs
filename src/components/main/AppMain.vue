e90c1e28549a085969f7e73e920f40fe446f56ab
sirio95 <sirio.iasevoli@gmail.com>
2022-12-21 18:39:51 +0100

    struttura componenti main

--- src/components/main/AppMain.vue
+++ src/components/main/AppMain.vue
@@ -1,9 +1,30 @@
 <script>
+    import ModernConstruction from './ModernConstruction.vue';
+    import RecentWork from './RecentWork.vue';
+    import OurValues from './OurValues.vue';
+    import Review from './Review.vue';
+    import LatestNews from './LatestNews.vue';
+    import TrustedPartners from './TrustedPartners.vue';
 
+    export default{
+        components:{
+            ModernConstruction,
+            RecentWork,
+            OurValues,
+            Review,
+            LatestNews,
+            TrustedPartners
+        }
+    }
 </script>
 
 <template>
-    
+    <ModernConstruction />
+    <RecentWork />
+    <OurValues />
+    <Review />
+    <LatestNews />
+    <TrustedPartners />
 </template>
 
 <style lang="scss">
