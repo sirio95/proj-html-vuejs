bcbaf94d40c21ec5293a188e95553a4930fd7264
sirio95 <sirio.iasevoli@gmail.com>
2022-12-22 11:50:51 +0100

    importato font-awesome

--- src/components/main/ModernConstruction.vue
+++ src/components/main/ModernConstruction.vue
@@ -1,9 +1,50 @@
 <script>
-
+    export default{
+        data(){
+            return{
+                cards: [
+                    {
+                        icon: 'fa-regular fa-building',
+                        name: 'buildings',
+                        text: 'Quis autem vel eum iure reprenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.' 
+                    },
+                    {
+                        icon: 'fa-solid fa-arrows-rotate',
+                        name: 'renovate',
+                        text: 'Quis autem vel eum iure reprenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.'
+                    },
+                    {
+                        icon: 'fa-solid fa-house-chimney',
+                        name: 'construct',
+                        text: 'Quis autem vel eum iure reprenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.'
+                    },
+                    {
+                        icon: 'fa-solid fa-truck',
+                        name: 'exclusive',
+                        text: 'Quis autem vel eum iure reprenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.'
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <template>
-    
+    <section>
+        <div>
+            <h2>specialist in modern construction</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita iusto error earum corporis voluptatem facere!</p>
+        </div>
+        <div>
+            <ul>
+                <li v-for="(element, index) in cards" :key="index">
+                    <font-awesome-icon :icon="element.icon" />
+                    <h2>{{ element.name }}</h2>
+                    <p>{{ element.text }}</p>
+                </li>
+            </ul>
+        </div>
+    </section>
 </template>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(fas, far)
 
-createApp(App).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
