0962ab31c93b45d94c396a74b09fff1869cfe663
sirio95 <sirio.iasevoli@gmail.com>
2022-12-23 03:39:17 +0100

    work component struttura

--- src/components/main/RecentWork.vue
+++ src/components/main/RecentWork.vue
@@ -1,9 +1,48 @@
 <script>
-
+    export default{
+        data(){
+            return{
+                works:[
+                    {
+                        name: 'florida health facility',
+                        img: 'project1-featured-294276386-400x400.jpg',
+                        type: 'commercial',
+                    },
+                    {
+                        name: 'berlin bundeskanzleramt',
+                        img: 'project2-featured-15013609-400x400.jpg',
+                        type: 'public'
+                    },
+                    {
+                        name: 'exclusive urban living',
+                        img: 'project3-featured-189023420-400x400.jpg',
+                        type: 'private'
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <template>
-    
+    <section>
+        <div v-for="(element, index) in works" :key="index" class="card">
+            <img src="../../assets/{{element.img}}" :alt="element.name">
+            <div class="hover-card">
+                <div>
+                    <font-awesome-icon icon="fa-solid fa-link" />
+                    <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                </div>
+                <h3>{{ element.name }}</h3>
+                <p>{{ element.type }}</p>
+            </div>
+        </div>
+        <div>
+            <div class="bar"></div>
+            View More
+            <div class="bar"></div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss">
