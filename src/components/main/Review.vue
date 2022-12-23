1fd44e5b134d42ca1941d637332ede232d55ee51
sirio95 <sirio.iasevoli@gmail.com>
2022-12-23 03:53:49 +0100

    struttura componente recensioni

--- src/components/main/Review.vue
+++ src/components/main/Review.vue
@@ -1,9 +1,26 @@
 <script>
+    export default{
+        data(){
+            return{
 
+            }
+        }
+    }
 </script>
 
 <template>
-    
+    <section>
+        <h2>our home owners say</h2>
+        <img src="../../assets/home-testimonial-113165296.jpg" alt="profilo cliente">
+        <p>
+            'No man but feels more of a man in the world if he have but a bit of ground that he can call his own. However small it is on the surface, it is four thousand miles deep; and that is a very handsome property.'
+        </p>
+        <h3>harry smith &middot; new home owner</h3>
+        <div>
+            <font-awesome-icon icon="fa-solid fa-circle" />
+            <font-awesome-icon icon="fa-regular fa-circle" />
+        </div>
+    </section>
 </template>
 
 <style lang="scss">
